b220a89557cd7d93bf7831a8528d940cd38b4fd1
olilusca <lodr130697@gmail.com>
2024-04-29 09:28:52 -0300

    Primeira versão da ContaTerminal (1.0)

---
+++ desafio-conta-terminal/ContaTerminal.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Criação de conta");
+        
+        System.out.println("Insira o nome da conta: ");
+        String nomeConta = input.next();
+        
+        System.out.println("Insira o número da conta: ");
+        int numeroConta = input.nextInt();
+        
+        System.out.println("Conta criada com sucesso!");
+        System.out.println();
+        System.out.println("Informações da conta:");
+        System.out.println("Nome: " + nomeConta);
+        System.out.println("Número: " + numeroConta);
+    }
+}
