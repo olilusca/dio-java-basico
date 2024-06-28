0bf030186522aa720404c8708d1cc40b4fa81c6d
olilusca <lodr130697@gmail.com>
2024-06-28 11:00:43 -0300

    Testes realizados na Main

--- collections-framework-java/SET/OperacoesBasicas/PalavrasUnicas/src/Main.java
+++ collections-framework-java/SET/OperacoesBasicas/PalavrasUnicas/src/Main.java
@@ -7,10 +7,11 @@ public class Main
 		PalavraSet palavraset = new PalavraSet();
 		
 		palavraset.addPalavra("Lucas");
-		palavraset.addPalavra("Maria");
+		palavraset.addPalavra("Luc");
 		System.out.println("Total in set: " + palavraset.exibirContagemPalavra());
 		palavraset.removerPalavra("Luca");
-		System.out.println("Exists? " + palavraset.verificarPalavra("Maria"));
+		System.out.println("Exists? " + palavraset.verificarPalavra("Lucas"));
+		palavraset.exibirPalavras();
 	
 	}
 }
